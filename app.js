d12d729fbcd1594111389d56f2466af90b6ae8ad
chattray <chea0073@gmail.com>
2021-10-20 09:54:07 -0700

    Testers updated

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const error500 = require('./src/error-handlers/500.js');
 app.use(express.json());
 app.use(logger);
 
-app.get('/person', validator, handlePerson);
+app.get('/person', validator,  handlePerson);
 
 function handlePerson(request, response) {
   console.log('handlePerson', request);
@@ -22,8 +22,8 @@ function handlePerson(request, response) {
   response.status(200).send(name);
 }
 
-app.use(error404);
 app.use(error500);
+app.use(error404);
 
 module.exports = {
   app,

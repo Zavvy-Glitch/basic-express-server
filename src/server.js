b5d219b3e962259c0eef93ab15b6c47a13e5dd68
Chattray <chattray.chea@gmail.com>
2022-03-22 13:21:29 -0700

    reconfigure server middleware order of precedence

--- src/server.js
+++ src/server.js
@@ -6,42 +6,41 @@ const express = require('express');
 const app = express();
 const logger = require('./middleware/logger.js');
 const validator = require('./middleware/validator.js')
-// const validator = require('./middleware/validator.js');
 const error404 = require('./error-handlers/404.js');
 const error500 = require('./error-handlers/500.js');
 
 app.use(express.json());
-app.use(logger);
 
 const handlePerson = require('../src/handlePerson.js')
 
 // Routes
 
 // retrieves all persons
-app.get('/person', logger, validator, handlePerson);
+app.get('/person', logger, handlePerson, validator);
 
 // retrieves ONE person
 app.get('/person/:id', () => {
-
+  
 });
 
 // creates a new person
 app.post('/person', () => {
-
+  
 });
 
 // updates a person
 app.put('/person/:id', () => {
-
+  
 });
 
 // deletes a person
 app.delete('/person/:id', () => {
-
+  
 });
 
+app.use(logger);
+app.use(validator);
 app.use(error404);
 app.use(error500);
 
 module.exports = app;
- 
